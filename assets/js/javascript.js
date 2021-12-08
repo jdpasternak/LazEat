1a1255072fc809bb639249ee36408dfeff473691
Jake Pasternak <jdp.pasternak@gmail.com>
2021-12-08 12:48:15 -1000

    Add unit-conversion module, add script tag to index.html

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,3 +1,6 @@
-fetch("api.edamam.com/api/nutrition-data")
-fetch("api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+import * as UnitConversion from "./unit-conversion.js";
 
+fetch("api.edamam.com/api/nutrition-data");
+fetch(
+  "api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+);
