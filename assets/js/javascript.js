7028ad7a5a52489e859e58c13379ee695b23c0b8
Afaed <44733498+Afaed@users.noreply.github.com>
2021-12-09 21:22:23 -0800

    merge code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,13 +1,39 @@
-var food = function () {
+var foodrec = document.getElementById("FoodReccomendation")
+var food = document.getElementById("Box")
+var recipe = function () {
+var recipe = {}
+$.ajax(setting2).then(function (response) {
+	response.json;
+ 
+});
+then(function(data)
+{
+	recipe = data
+})
+document.createElement("div")
+return recipe //a json object
 
 }
-var weather = function (current) {
+var weather = function () {
+	$.ajax(setting1).then(function (response) {
+		response.json;
+	 
+	});
+	then(function(data)
+	{
+		weather = data
+	})
+	return weather //a json object
 
 }
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
 
+  var ingredients 
 //api
 
-fetch("api.edamam.com/api/nutrition-data")
+
 const setting1 = {
     "async": true,
 	"crossDomain": true,
@@ -16,23 +42,10 @@ const setting1 = {
 	"headers": {
 		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
 		"x-rapidapi-key": "27f14b1cebmsh5a0059667e11c37p13c476jsn80efdba32824"
-	}
-
 }
-fetch("api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+}
 
 const setting2 = {
-    "async": true,
-	"crossDomain": true,
-	"url": "api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}",
-	"method": "POST",
-	"headers": {
-		"x-rapidapi-host": "mycookbook-io1.p.rapidapi.com",
-		"x-rapidapi-key": "27f14b1cebmsh5a0059667e11c37p13c476jsn80efdba32824"
-}
-}
-
-const setting3 = {
 	"async": true,
 	"crossDomain": true,
 	"url": "www.jamieoliver.com/recipes/vegetables-recipes/superfood-salad/",
@@ -50,7 +63,6 @@ $.ajax(setting1).done(function (response) {
 $.ajax(setting2).done(function (response) {
 	console.log(response);
 });
-$.ajax(setting3).done(function (response) {
-	console.log(response);
-});
 
+
+FoodRec.addEventListener("click", recipe)
